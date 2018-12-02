64e47e6a65f6eb79a3f4dd28088633800969db4f
michaelrauh <michaelrauh@ymail.com>
2018-12-01 20:42:56 -0500

    part two

--- one.py
+++ one.py
@@ -1,8 +1,18 @@
 with open("input1.txt", "r") as f:
     lines = f.read()
 
+seen = set()
 acc = 0
 split = lines.split('\n')
 split.pop()
 numbers = [int(i) for i in split]
-print(sum(numbers))
+runs = 0
+while True:
+    runs += 1
+    print(runs)
+    for number in numbers:
+        acc += number
+        if acc in seen:
+            print(acc)
+            exit()
+        seen.add(acc)
