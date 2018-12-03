98b17be133a6997ad98e141183cfa975c2acc683
michaelrauh <michaelrauh@ymail.com>
2018-12-02 19:29:39 -0500

    another hack for the second star

--- two.py
+++ two.py
@@ -9,15 +9,13 @@ triples = 0
 for id in ids:
     chars = list(id)
     counts = {}
-    for char in chars:
-        if char not in counts:
-            counts[char] = 1
-        else:
-            counts[char] += 1
-    all_counts = set(counts.values())
-    if 2 in all_counts:
-        doubles += 1
-    if 3 in all_counts:
-        triples += 1
-
-print(doubles * triples)
+    for second_id in ids:
+        in_common = 0
+        second_chars = list(second_id)
+        for i in range(len(chars)):
+            if chars[i] == second_chars[i]:
+                in_common += 1
+        if in_common == len(chars) -1:
+            print(id)
+            print(second_id)
+            exit()
